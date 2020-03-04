675f7fe65ed2265f0e6d1b0ebf7b9db3f6d06f6d
Campos Forcen, Jesus <29117658@ibercaja.es>
2020-03-04 14:54:29 +0100

    Add project files.

---
+++ MiServicio/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace MiServicio.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Config()
+        {
+            return new List<string>
+            {
+                configuration.GetSection("Audit").GetValue<string>("ConnectionString"),
+                configuration.GetValue<string>("InstrumentationKey")
+            };
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Env()
+        {
+            return new List<string>
+            {
+                System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+                System.Environment.GetEnvironmentVariable("Audit:ConnectionString"),
+                System.Environment.GetEnvironmentVariable("InstrumentationKey")
+            };
+        }
+    }
+}
---
+++ MiServicio/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MiServicio
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            logger.LogInformation($"Configuring Pipeline for Environment: {env.EnvironmentName}");
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation($"context.Request.Host: {context.Request.Host}");
+                logger.LogInformation($"context.Request.Path: {context.Request.Path}");
+                logger.LogInformation($"context.Request.Scheme: {context.Request.Scheme}");
+                logger.LogInformation($"context.Request.PathBase: {context.Request.PathBase}");
+                await next.Invoke();
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            logger.LogInformation($"Pipeline configured");
+
+        }
+    }
+}
